56782def139cb696550e926d0c750a8fde235c13
hyejin <wls5258@jbnu.ac.kr>
2023-01-25 16:33:50 +0900

    중복되는 변수 수정

--- camera.py
+++ camera.py
@@ -3,24 +3,20 @@ from datetime import datetime
 from picamera import PiCamera
 from time import sleep
 
-
 output_dir = "/home/pi/camera/images"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
-
-
 
 camera = PiCamera()
 camera.vflip = True
 camera.hflip = True
 camera.start_preview()
 
-#for i in range(10):
+# for i in range(10):
 #   sleep(10)
 #   camera.capture('/home/pi/image%s.jpg' % i)
 
 
-date_str = "20230104"
 now = datetime.now()
 date_str = now.strftime("%Y%m%d_%H%M")
 sleep(5)
